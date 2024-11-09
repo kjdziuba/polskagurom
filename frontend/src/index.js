4323eedce890a6e5d23b1feede93ba7ebbaf0a7b
Krzysztof Dziuba <krzysztof.dziuba@manchester.ac.uk>
2024-11-09 21:34:20 +0000

    making sure okay and resetting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-ooe86kjwycuzft4q.us.auth0.com"
+    clientId="uowWTaieSdNAcMj7vS4XH4GtIPoyZQoK"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
