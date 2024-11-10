ec1533f4a7cea8098e0ae98d6f46b85f3116ed40
Krzysztof Dziuba <krzysztof.dziuba@manchester.ac.uk>
2024-11-10 02:44:19 +0000

    formatting code in HomeWeb.js

--- frontend/src/pages/HomeWeb.js
+++ frontend/src/pages/HomeWeb.js
@@ -4,39 +4,38 @@ import { Chart, CategoryScale, LinearScale, registerables } from "chart.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NetWorth from "../components/NetWorth";
 import FutureMe from "../components/FutureMe";
-import {Spending} from "../components/Spending";
+import { Spending } from "../components/Spending";
 import News from "../components/News";
 import Notifications from "../components/Notifications";
 import "../styles/style.css";
 Chart.register(...registerables);
 
-
 function HomeWeb() {
   return (
     <div className="container">
-    <h1 className="text-center my-4">Timewise Finance</h1>
-    <div className="row">
-      <div className="col-md-6">
-        <NetWorth />
+      <h1 className="text-center my-4">Timewise Finance</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <NetWorth />
+        </div>
+        <div className="col-md-6">
+          <Spending />
+        </div>
       </div>
-      <div className="col-md-6">
-        <Spending />
+      <div className="row">
+        <div className="col-md-6">
+          <News />
+        </div>
+        <div className="col-md-6">
+          <Notifications />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <FutureMe />
+        </div>
       </div>
     </div>
-    <div className="row">
-      <div className="col-md-6">
-        <News />
-      </div>
-      <div className="col-md-6">
-        <Notifications />
-      </div>
-    </div>
-    <div className="row">
-      <div className="col">
-        <FutureMe />
-      </div>
-    </div>
-  </div>
   );
 }
 
